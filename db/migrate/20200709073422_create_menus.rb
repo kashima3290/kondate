b67b513c2cc490b01e3a481048507f0493a4833e
kashimaraiki <raiki3290@gmail.com>
2020-07-09 17:25:26 +0900

    db:migrate

--- db/migrate/20200709073422_create_menus.rb
+++ db/migrate/20200709073422_create_menus.rb
@@ -1,7 +1,10 @@
 class CreateMenus < ActiveRecord::Migration[6.0]
   def change
     create_table :menus do |t|
-
+      t.string :name, null: false
+      t.string :genre, null: false
+      t.string :site
+      t.text :text
       t.timestamps
     end
   end
