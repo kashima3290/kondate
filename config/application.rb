e8eb192cc1938ef5610ce4ada5320db6e438fc1c
kashimaraiki <raiki3290@gmail.com>
2020-07-10 15:16:40 +0900

    view collapse prevention

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Kondate
   class Application < Rails::Application
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
