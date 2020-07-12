b0b8f0dbac8a6b2593a19e2b0fca56a9b6a5aa8c
kashimaraiki <raiki3290@gmail.com>
2020-07-12 18:00:20 +0900

    root login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    root "users/sessions#new"
+  end
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: "users/sessions",
     passwords: "users/passwords"
   }
-  root 'menus#index'
+  resources 'menus'
 end
