bb646eb88bfe97ef572a29fbecd1da40f2fa6e3e
kashimaraiki <raiki3290@gmail.com>
2020-07-13 20:01:19 +0900

    devise authenticate_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    root "users/sessions#new"
-  end
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: "users/sessions",
     passwords: "users/passwords"
   }
-  resources 'menus'
+  root "menus#index"
+  resources :menus
 end
