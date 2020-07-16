23ba4b70e54a658684c13b0947895254aaebeec1
kashimaraiki <raiki3290@gmail.com>
2020-07-16 21:29:35 +0900

    tubolinks jQuery reload

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,13 @@ require("channels")
 require("semantic-ui-sass")
 require('jquery')
 
+jQuery(document).on('turbolinks:load', function(){
+  $('.ui.dropdown').dropdown();
+  $('.special.cards .image').dimmer({
+    on: 'hover'
+  });
+})
+
 $(function(){
   $('.ui.dropdown').dropdown();
   $('.special.cards .image').dimmer({
