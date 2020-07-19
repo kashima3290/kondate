60f40418c094165da5a3c83d49eb18b60a8cff16
kashimaraiki <raiki3290@gmail.com>
2020-07-19 16:06:16 +0900

    user name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
   has_one_attached :icon
 end
