94576ff8bd34a3f3f6db2994b11067d4212242ff
kashimaraiki <raiki3290@gmail.com>
2020-07-19 16:58:39 +0900

    user edit error message

--- db/migrate/20200709073626_devise_create_users.rb
+++ db/migrate/20200709073626_devise_create_users.rb
@@ -4,8 +4,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name
-      t.text :text
+      t.string :name, null: false
+      t.text :profile
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
