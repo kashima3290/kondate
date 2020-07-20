ecf67f9a71f17c864c762a6825f3fa57a0ef6208
kashimaraiki <raiki3290@gmail.com>
2020-07-20 21:49:36 +0900

    strict: false

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,10 +9,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     sign_in(@user)
   end
 
-  # test "should get index" do
-  #   get users_index_url
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get users_index_url
+    assert_response :success
+  end
 
   # test "should get show" do
   #   get users_show_url
