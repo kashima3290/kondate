0037046d980271a10080abdc0552df522bcf5b47
kashimaraiki <raiki3290@gmail.com>
2020-09-27 20:22:07 +0900

    get menu data id

--- app/javascript/packs/preview_menus.js
+++ app/javascript/packs/preview_menus.js
@@ -1,7 +1,8 @@
 document.addEventListener("turbolinks:load", function(){
   // fileの中の画像が変更さえたときに、そのinputタグのカスタムデータ属性を取得
-  $('change-file').on('change', function(e) {
-    
+  $('.change-file').on('change', function(event) {
+    let data_id = event.target.getAttribute('data-menu-file-id');
+    console.log(data_id);
+    // 中のsrcを入れ替える
   })
-  // 中のsrcを入れ替える
 })
