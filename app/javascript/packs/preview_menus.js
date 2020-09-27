7d445c41dda7aae2d08148ee30dc06c992b4bdee
kashimaraiki <raiki3290@gmail.com>
2020-09-27 19:38:51 +0900

    custom data name change

--- app/javascript/packs/preview_menus.js
+++ app/javascript/packs/preview_menus.js
@@ -1,3 +1,7 @@
 document.addEventListener("turbolinks:load", function(){
-  console.log('hello')
+  // fileの中の画像が変更さえたときに、そのinputタグのカスタムデータ属性を取得
+  $('change-file').on('change', function(e) {
+    
+  })
+  // 中のsrcを入れ替える
 })
