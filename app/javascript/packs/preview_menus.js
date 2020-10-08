49fa670ac20af3b15610db01d37a3665c7de6dfd
kashimaraiki <raiki3290@gmail.com>
2020-10-08 22:19:33 +0900

    Suspended due to deployment work

--- app/javascript/packs/preview_menus.js
+++ app/javascript/packs/preview_menus.js
@@ -2,7 +2,10 @@ document.addEventListener("turbolinks:load", function(){
   // fileの中の画像が変更さえたときに、そのinputタグのカスタムデータ属性を取得
   $('.change-file').on('change', function(event) {
     let data_id = event.target.getAttribute('data-menu-file-id');
-    console.log(data_id);
-    // 中のsrcを入れ替える
+    console.log(event.target);
+    // idが同じimageのsrcを入れ替える
+    // let image = document.querySelector('.change-image')
+    // console.log(image.dataset.menuImageId)
+    // ボタンを変更・削除ができるように編集
   })
 })
