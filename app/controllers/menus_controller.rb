02de9075dc8c6159413adbe5bb0647fb7aa84779
kashimaraiki <raiki3290@gmail.com>
2020-10-31 15:56:12 +0900

    menu validation

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -32,6 +32,6 @@ class MenusController < ApplicationController
 
   private
   def menu_params
-    params.require(:menu).permit(:name, :genre, :text, menu_images_attributes: [:image]).merge(user_id: current_user.id)
+    params.require(:menu).permit(:name, :genre, :site, :text, menu_images_attributes: [:image]).merge(user_id: current_user.id)
   end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,4 +2,6 @@ class Menu < ApplicationRecord
   has_many :menu_images, dependent: :destroy
   accepts_nested_attributes_for :menu_images
   belongs_to :user
+  validates :menu_images, presence: true
+  validates :name, presence: true
 end
