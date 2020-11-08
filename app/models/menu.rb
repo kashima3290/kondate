99d4239afb68d4094622c6c2ece80762b4468676
kashimaraiki <raiki3290@gmail.com>
2020-11-08 17:01:51 +0900

    Update menu.rb

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,6 @@
 class Menu < ApplicationRecord
-  has_many :menu_images, dependent: :destroy
-  accepts_nested_attributes_for :menu_images
+  has_many :menu_images
+  accepts_nested_attributes_for :menu_images, allow_destroy: true
   belongs_to :user
   validates :menu_images, presence: true
   validates :name, presence: true
