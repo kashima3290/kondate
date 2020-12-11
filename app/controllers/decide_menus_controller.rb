5f2ce5294111dcf195495b007a63895439a93677
kashimaraiki <raiki3290@gmail.com>
2020-12-11 20:28:18 +0900

    Update decide_menus_controller.rb

--- app/controllers/decide_menus_controller.rb
+++ app/controllers/decide_menus_controller.rb
@@ -3,8 +3,11 @@ class DecideMenusController < ApplicationController
   def index
   end
 
-  def random_menu
-    @random_menu = Menu.includes(:user).order("RAND()").first
+  def random_menu(genre = "")
+    # 引数が空なら全てのメニューから選択
+    if genre = ""
+      @random_menu = Menu.includes(:user).order("RAND()").first
+    end
   end
 
 end
