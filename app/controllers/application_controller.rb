f892e35252bcd39e411f6323991db21a9bddb773
kashimaraiki <raiki3290@gmail.com>
2020-12-24 17:55:19 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :menu_search
 
   protected
 
@@ -9,5 +10,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+
+  def menu_search
+    @search = Menu.ransack(params[:q]) # 検索オブジェクトを生成
+    @menuss = @search.result.includes(:menu_images).order(created_at: :desc).page(params[:page]).per(8)
   end
 end
