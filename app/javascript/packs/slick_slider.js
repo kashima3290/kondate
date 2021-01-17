be8718a3915c037f32d2655f9a5abe232001e2e2
kashimaraiki <raiki3290@gmail.com>
2021-01-17 19:47:42 +0900

    Update slick_slider.js

--- app/javascript/packs/slick_slider.js
+++ app/javascript/packs/slick_slider.js
@@ -1,14 +1,14 @@
-// document.addEventListener("turbolinks:load", function(){
-//   $('.slider').slick({
-//       arrows: true,
-//       prevArrow:'<i class="angle left big icon"></i>',
-//       nextArrow:'<i class="angle right big icon"></i>',
-//       dots: true,
-//       autoplay: true,
-//       autoplaySpeed: 4000,
-//       speed: 800
-//   });
-//   $('.slick-dots li').on('mouseover', function() {
-//     $('.slider').slick('goTo', $(this).index());
-//   });
-// });
+document.addEventListener("turbolinks:load", function(){
+  $('.slider').slick({
+      arrows: true,
+      prevArrow:'<i class="angle left big icon"></i>',
+      nextArrow:'<i class="angle right big icon"></i>',
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      speed: 800
+  });
+  $('.slick-dots li').on('mouseover', function() {
+    $('.slider').slick('goTo', $(this).index());
+  });
+});
