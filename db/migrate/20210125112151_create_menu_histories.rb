335a098a4ed99c167621ff96a0485f04b69f76ef
kashimaraiki <raiki3290@gmail.com>
2021-02-07 12:02:32 +0900

    menu_history table update

--- db/migrate/20210125112151_create_menu_histories.rb
+++ db/migrate/20210125112151_create_menu_histories.rb
@@ -2,10 +2,9 @@ class CreateMenuHistories < ActiveRecord::Migration[6.0]
   def change
     create_table :menu_histories do |t|
       t.references :user, foreign_key: true, null: false
-      t.integer :menu_id, null: false
+      t.references :menu, foreign_key: true, null: false
       t.timestamp :eating_date, null: false
       t.timestamps
     end
-    add_index :menu_histories, :menu_id
   end
 end
