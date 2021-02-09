375469ab6a9fc515570f8c86cd69186ea7de78a4
kashimaraiki <raiki3290@gmail.com>
2021-02-09 19:35:52 +0900

    Update 20210208032227_create_menu_menu_histories_connections.rb

--- db/migrate/20210208032227_create_menu_menu_histories_connections.rb
+++ db/migrate/20210208032227_create_menu_menu_histories_connections.rb
@@ -1,8 +1,8 @@
 class CreateMenuMenuHistoriesConnections < ActiveRecord::Migration[6.0]
   def change
     create_table :menu_menu_histories_connections do |t|
-      t.references :menu
-      t.references :menu_histories
+      t.references :menu, foreign_key: true, null: false
+      t.references :menu_histories, foreign_key: true, null: false
       t.timestamps
     end
   end
