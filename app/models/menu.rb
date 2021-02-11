d6110353caddb6e03ea74be11dcdd9386972cd3b
kashimaraiki <raiki3290@gmail.com>
2021-02-11 11:24:31 +0900

    Update menu.rb

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,7 @@
 class Menu < ApplicationRecord
   has_many :menu_images, dependent: :destroy
-  has_many :menu_menu_histories_connections, dependent: :destroy
+  has_many :menu_menu_histories_connections, dependent: :destroy #メニューが削除された時は食べた履歴と繋ぐ中間データも削除
+  has_many :menu_histories, through: :menu_menu_histories_connections
   accepts_nested_attributes_for :menu_images, allow_destroy: true
   belongs_to :user
   validates :name, presence: true
