9e8f3f936e2b37992e60ed6d7d490fc57c906c66
kashimaraiki <raiki3290@gmail.com>
2021-02-16 21:38:47 +0900

    Update decide_menus_controller.rb

--- app/controllers/decide_menus_controller.rb
+++ app/controllers/decide_menus_controller.rb
@@ -1,6 +1,14 @@
 class DecideMenusController < ApplicationController
 
   def index
+    @random_menu_id = current_user.menus.order("RAND()").first
+    @staple_food_menu_id = current_user.menus.where(genre: '主食').order("RAND()").first
+    @main_dish_menu_id = current_user.menus.where(genre: '主菜').order("RAND()").first
+    @vegetable_menu_id = current_user.menus.where(genre: '野菜').order("RAND()").first
+    @one_item_menu_id = current_user.menus.where(genre: '一品もの').order("RAND()").first
+    @dessert_menu_id = current_user.menus.where(genre: 'デザート').order("RAND()").first
+    @other_menu_id = current_user.menus.where(genre: 'その他').order("RAND()").first
+    @eating_out_menu_id = current_user.menus.where(genre: '外食').order("RAND()").first
   end
 
   def random_menu
