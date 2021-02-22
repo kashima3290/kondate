f5a29fb3e2439bc0c57e85e4a361741977d35636
kashimaraiki <raiki3290@gmail.com>
2021-02-23 07:04:56 +0900

    Update decide_menus_controller.rb

--- app/controllers/decide_menus_controller.rb
+++ app/controllers/decide_menus_controller.rb
@@ -1,14 +1,15 @@
 class DecideMenusController < ApplicationController
 
   def index
-    @random_menu_id = current_user.menus.order("RAND()").first
-    @staple_food_menu_id = current_user.menus.where(genre: '主食').order("RAND()").first
-    @main_dish_menu_id = current_user.menus.where(genre: '主菜').order("RAND()").first
-    @vegetable_menu_id = current_user.menus.where(genre: '野菜').order("RAND()").first
-    @one_item_menu_id = current_user.menus.where(genre: '一品もの').order("RAND()").first
-    @dessert_menu_id = current_user.menus.where(genre: 'デザート').order("RAND()").first
-    @other_menu_id = current_user.menus.where(genre: 'その他').order("RAND()").first
-    @eating_out_menu_id = current_user.menus.where(genre: '外食').order("RAND()").first
+    @random_menu = current_user.menus.order("RAND()").first
+    @staple_food_menu = current_user.menus.where(genre: '主食').order("RAND()").first
+    @main_dish_menu = current_user.menus.where(genre: '主菜').order("RAND()").first
+    @vegetable_menu = current_user.menus.where(genre: '野菜').order("RAND()").first
+    @one_item_menu = current_user.menus.where(genre: '一品もの').order("RAND()").first
+    @dessert_menu = current_user.menus.where(genre: 'デザート').order("RAND()").first
+    @other_menu = current_user.menus.where(genre: 'その他').order("RAND()").first
+    @eating_out_menu = current_user.menus.where(genre: '外食').order("RAND()").first
+    @decide_menus = [@random_menu, @staple_food_menu, @main_dish_menu, @vegetable_menu, @main_dish_menu, @vegetable_menu, @one_item_menu, @dessert_menu, @other_menu, @eating_out_menu]
   end
 
   def random_menu(genre = "")
